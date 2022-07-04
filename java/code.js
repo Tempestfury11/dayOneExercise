4a939899e12124249dcdbc4cfe60e07aea1f3152
Tempestfury11 <Swiftyblue11@gmail.com>
2022-07-04 13:22:13 +0200

    https://github.com/Tempestfury11/dayOneExercise

--- java/code.js
+++ java/code.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
     data:function() {
         return {
-            CodeName : 'Marshalino Jankowski'
+            name : 'Marshalino',
+            surname : 'Jankowski',
         }
     }
 });
